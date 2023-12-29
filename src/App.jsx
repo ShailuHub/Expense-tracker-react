92b30e8c4e18f9f4fe8dd83789cf88f2fb295a99
Shailu <shailesh.respond@gmail.com>
2023-12-29 09:10:50 +0530

    Initialize expense tracker project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header";
+import AuthenticationForm from "./components/Authentication/AuthenticationForm";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <AuthenticationForm />
+    </>
+  );
+};
+
+export default App;
